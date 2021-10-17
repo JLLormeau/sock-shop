cf49fd2e06b0866c6d5b1c784e7a4586ec1357de
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2021-10-17 20:07:18 +0200

    Create deploy-sock-shop-on-microk8s.sh

---
+++ deploy-sock-shop-on-microk8s.sh
@@ -0,0 +1,35 @@
+export user=`whoami`
+
+sudo apt udpate
+sudo apt install snapd
+sudo apt install parter
+sudo apt install docker.io
+sudo apt install python3-jmespath
+sudo apt install ntp
+sudo apt install vim
+sudo apt install tree
+
+sudo snap install microk8s --classic --channel=1.22/stable
+sudo usermod -a -G microk8s $user
+sudo chown -f -R $user ~/.kube
+newgrp microk8s
+
+
+/snap/bin/microk8s.kubectl
+
+bash -c "echo \"--allow-privileged=true\" >> /var/snap/microk8s/current/args/kube-apiserver"
+sudo snap alias microk8s.kubectl kubectl
+#==> Liste add on /snap/bin/microk8s.status --wait-ready
+#/snap/bin/microk8s.enable dashboard
+#/snap/bin/microk8s.enable rbac
+/snap/bin/microk8s.enable dns
+/snap/bin/microk8s.enable storage
+/snap/bin/microk8s.enable registry
+/snap/bin/microk8s.enable ingress
+
+
+/snap/bin/microk8s.start
+/snap/bin/microk8s.status --wait-ready
+
+kubectl create namespace sock-shop
+kubectl apply -f complete-demo.yaml
