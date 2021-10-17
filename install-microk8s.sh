a632ea2cc8abf09d2165ed27640164abc3be83f4
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2021-10-17 22:44:09 +0200

    Update install-microk8s.sh

--- install-microk8s.sh
+++ install-microk8s.sh
@@ -16,21 +16,5 @@ sudo apt -y install tree
 sudo snap install microk8s --classic --channel=1.22/stable
 
 #add privilege on local user
-#sudo usermod -a -G microk8s $user
+sudo usermod -a -G microk8s $user
 sudo chown -f -R $user ~/.kube
-newgrp microk8s
-
-#alias kubecetl
-sudo snap alias microk8s.kubectl kubectl
-
-#add on
-/snap/bin/microk8s.enable dns
-/snap/bin/microk8s.enable storage
-/snap/bin/microk8s.enable registry
-/snap/bin/microk8s.enable ingress
-
-#start
-/snap/bin/microk8s.start
-/snap/bin/microk8s.status --wait-ready
-
-
