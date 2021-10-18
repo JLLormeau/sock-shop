7c5caa2ff32242ea806f513ec0dfd9bc1097c22d
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2021-10-18 21:46:18 +0200

    Update install-microk8s.sh

--- install-microk8s.sh
+++ install-microk8s.sh
@@ -22,21 +22,4 @@ echo "**********usermod"
 sudo usermod -a -G microk8s $user
 sudo chown -f -R $user ~/.kube
 echo "**********newgrp"
-newgrp microk8s &
-
-#alias kubectl
-echo "**********alias kubectl"
-sudo snap alias microk8s.kubectl kubectl
-
-#add on
-echo "**********add on"
-sudo /snap/bin/microk8s.enable dns
-sudo /snap/bin/microk8s.enable storage
-sudo /snap/bin/microk8s.enable registry
-sudo /snap/bin/microk8s.enable ingress
-
-#start
-echo "**********start microk8s...."
-sudo /snap/bin/microk8s.start
-sudo /snap/bin/microk8s.status --wait-ready
-
+newgrp microk8s
